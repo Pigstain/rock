0bf13476e70ddcafdc4fdc851292a3b4c04b6773
Pigstain <edelwiss2@gmail.com>
2023-06-13 20:02:05 +0300

    rock-ui finished

--- rock.js
+++ rock.js
@@ -1,4 +1,5 @@
-
+let pc_wins=0;
+let p_wins=0;
 
 function getComputerChoice(){
     var result = Math.floor(Math.random()*3);
@@ -17,37 +18,53 @@ function getPlayerChoice(){
 }
 
 
-function playRound(playerSelection,computerSelection){
-    playerSelection= playerSelection.toUpperCase();
+function playRound(e){
+    let playerSelection=e;
+    
+    let computerSelection=getComputerChoice();
     computerSelection= computerSelection.toUpperCase();
+    const display = document.querySelector(".display");
+    const screen = document.querySelector(".screen");
+    const screen_p = document.querySelector(".screen-p");
     if(playerSelection===computerSelection){
-        return "Draw!"
+        display.innerHTML="Draw!";
     }
     else{
         switch (playerSelection) {
             case "ROCK":
                 if(computerSelection=="PAPER"){
-                    return "You Lose!Paper beats Rock";
+                    display.innerHTML="You Lose!Paper beats Rock";
+                    screen.innerHTML=++pc_wins;
                 }
                 else{
-                    return "You Win! Rock beats Scissors";
+                    display.innerHTML="You Win! Rock beats Scissors";
+                    screen_p.innerHTML=++p_wins;
                 }
+                break;
             case "PAPER":
                 if(computerSelection=="ROCK"){
-                    return "You Win! Paper beats Rock";
+                    display.innerHTML="You Win! Paper beats Rock";
+                    screen_p.innerHTML=++p_wins;
                 }
                 else{
-                    return "You Lose! Scissors beats Paper";
+                    display.innerHTML="You Lose! Scissors beats Paper";
+                    screen.innerHTML=++pc_wins;
                 }
+                break;
             case "SCISSORS":
                 if(computerSelection=="ROCK"){
-                    return "You Lose! Rock beats Scissors";
+                    display.innerHTML="You Lose! Rock beats Scissors";
+                    screen.innerHTML=++pc_wins;
                 }
                 else{
-                    return "You Win! Scissors beats Paper";
+                    display.innerHTML="You Win! Scissors beats Paper";
+                    screen_p.innerHTML=++p_wins;
                 }
+                break;
         }
     }
+    check();
+    return;
 }
 function game(){
     let count =5;
@@ -58,4 +75,32 @@ function game(){
         count--;
     }
 }
-game();
+function temp(){
+    console.log('HEY');
+}
+const button = document.querySelectorAll('.button');
+button.forEach(button => {
+   
+
+    button.addEventListener("click",()=>playRound(button.textContent));
+    
+});
+function check(){
+if(pc_wins==5|| p_wins==5){
+    const screen = document.querySelector(".screen");
+    const screen_p = document.querySelector(".screen-p");
+    if(p_wins===5){
+    window.alert("YOU WONS");
+        screen.innerHTML=0;
+        screen_p.innerHTML=0;
+    }
+    else{
+        window.alert("PC WON");
+        screen.innerHTML=0;
+        screen_p.innerHTML=0;
+    }
+    p_wins=0;
+    pc_wins=0;
+}
+}
+//game();
